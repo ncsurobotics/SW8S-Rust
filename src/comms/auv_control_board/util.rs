0cac4247323feba67a3b680598b758f7000c370d
Bennett Petzold <bennettpetzold01@gmail.com>
2023-09-28 17:32:10 -0400

    Copy CRC implementation

--- src/comms/auv_control_board/util.rs
+++ src/comms/auv_control_board/util.rs
@@ -6,8 +6,20 @@ pub const START_BYTE: u8 = 253;
 pub const END_BYTE: u8 = 254;
 pub const ESCAPE_BYTE: u8 = 255;
 
-pub fn crc(_bytes: &[u8]) -> u16 {
-    todo!()
+/// See https://github.com/ncsurobotics/SW8S-Java/blob/main/app/src/main/java/org/aquapackrobotics/sw8s/comms/CRC.java
+pub fn crc(bytes: &[u8]) -> u16 {
+    let mut crc = 0xFFFF;
+    bytes.iter().for_each(|byte| {
+        (0..8).for_each(|idx| {
+            let bit: u8 = ((byte >> (7 - idx) & 1) == 1).into();
+            let c15: u8 = ((crc >> 15 & 1) == 1).into();
+            crc <<= 1;
+            if (c15 ^ bit) != 0 {
+                crc ^= 0x1021;
+            }
+        })
+    });
+    crc
 }
 
 #[derive(Debug)]
