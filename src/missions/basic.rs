919cf58481c75e34743a21b2894a0577004b844a
Bennett Petzold <bennettpetzold01@gmail.com>
2023-10-16 19:03:17 -0400

    Update example Action

--- src/missions/basic.rs
+++ src/missions/basic.rs
@@ -1,9 +1,12 @@
 use super::{
-    action::{Action, ActionSequence},
+    action::{Action, ActionConcurrent, ActionParallel, ActionSequence},
     meb::WaitArm,
+    movement::Descend,
 };
-use anyhow::Result;
 
-fn initial_descent<T, U, V>(context: &T) -> ActionSequence<T, T, WaitArm<T>, WaitArm<T>> {
-    ActionSequence::new(WaitArm::new(context), WaitArm::new(context))
+fn initial_descent<T: Send + Sync>(context: &T) -> impl Action + '_ {
+    ActionSequence::<T, T, _, _>::new(
+        ActionParallel::<T, T, _, _>::new(WaitArm::new(context), Descend::new(context, -0.5)),
+        ActionConcurrent::<T, T, _, _>::new(WaitArm::new(context), Descend::new(context, -1.0)),
+    )
 }
