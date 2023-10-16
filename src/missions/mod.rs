d5fad0f9cbe8856ef79069de17694c5a712dec6a
Bennett Petzold <bennettpetzold01@gmail.com>
2023-10-16 15:37:48 -0400

    Remove ActionContext trait and debug bounds

--- src/missions/mod.rs
+++ src/missions/mod.rs
@@ -1,54 +1,51 @@
 use async_trait::async_trait;
 use core::fmt::Debug;
 use std::marker::PhantomData;
-use tokio::{join, try_join, io::AsyncWriteExt};
+use tokio::{io::AsyncWriteExt, join, try_join};
 
 use crate::comms::{control_board::ControlBoard, meb::MainElectronicsBoard};
-
-
-
-/**
- * Trait that signifies a struct is an action dependency. 
- */
-pub trait ActionContext {}
 
 /**
  * Inherit this trait if you have a control board
  */
-pub trait get_control_board<T: AsyncWriteExt + Unpin> {
+pub trait GetControlBoard<T: AsyncWriteExt + Unpin> {
     fn get_control_board(&self) -> &ControlBoard<T>;
 }
 
 /**
  * Inherit this trait if you have a MEB
  */
-pub trait get_main_electronics_board {
+pub trait GetMainElectronicsBoard {
     fn get_main_electronics_board(&self) -> &MainElectronicsBoard;
 }
 
+#[derive(Debug)]
 struct EmptyActionContext;
 
-impl ActionContext for EmptyActionContext {}
-
-struct FullActionContext<T: AsyncWriteExt + Unpin> {
+struct FullActionContext<T: AsyncWriteExt + Unpin + Send> {
     control_board: ControlBoard<T>,
     main_electronics_board: MainElectronicsBoard,
 }
-impl ActionContext for FullActionContext<tokio_serial::SerialStream> {}
-impl<T: AsyncWriteExt + Unpin> FullActionContext<T> {
-    const fn new(control_board: ControlBoard<T>, main_electronics_board: MainElectronicsBoard) -> Self {
+
+impl<T: AsyncWriteExt + Unpin + Send> FullActionContext<T> {
+    const fn new(
+        control_board: ControlBoard<T>,
+        main_electronics_board: MainElectronicsBoard,
+    ) -> Self {
         Self {
             control_board,
             main_electronics_board,
         }
     }
 }
-impl get_control_board<tokio_serial::SerialStream> for FullActionContext<tokio_serial::SerialStream> {
+
+impl GetControlBoard<tokio_serial::SerialStream> for FullActionContext<tokio_serial::SerialStream> {
     fn get_control_board(&self) -> &ControlBoard<tokio_serial::SerialStream> {
         &self.control_board
     }
 }
-impl get_main_electronics_board for FullActionContext<tokio_serial::SerialStream> {
+
+impl GetMainElectronicsBoard for FullActionContext<tokio_serial::SerialStream> {
     fn get_main_electronics_board(&self) -> &MainElectronicsBoard {
         &self.main_electronics_board
     }
@@ -58,16 +55,14 @@ impl get_main_electronics_board for FullActionContext<tokio_serial::SerialStream
  * A trait for an action that can be executed.
  */
 #[async_trait]
-pub trait Action<Output: Debug + Send + Sync>: Debug + Send + Sync + Sync {
+pub trait Action<Output: Send + Sync>: Send + Sync {
     async fn execute(self) -> Output;
 }
 
 /**
  * A trait that can be executed and modified at runtime.  
  */
-pub trait ActionMod<Input: ActionContext, Output: Debug + Send + Sync>:
-    Action<Output>
-{
+pub trait ActionMod<Input: Send + Sync, Output: Send + Sync>: Action<Output> {
     fn modify(&mut self, input: Input);
 }
 
@@ -75,7 +70,7 @@ pub trait ActionMod<Input: ActionContext, Output: Debug + Send + Sync>:
  * An action that runs one of two actions depending on if its conditional reference is true or false.  
  */
 #[derive(Debug)]
-pub struct ActionConditional<U: ActionContext, V: Action<bool>, W: Action<U>, X: Action<U>> {
+pub struct ActionConditional<U: Send + Sync, V: Action<bool>, W: Action<U>, X: Action<U>> {
     condition: V,
     true_branch: W,
     false_branch: X,
@@ -85,9 +80,7 @@ pub struct ActionConditional<U: ActionContext, V: Action<bool>, W: Action<U>, X:
 /**
  * Implementation for the ActionConditional struct.  
  */
-impl<U: Debug + Send + Sync, V: Action<bool>, W: Action<U>, X: Action<U>>
-    ActionConditional<U, V, W, X>
-{
+impl<U: Send + Sync, V: Action<bool>, W: Action<U>, X: Action<U>> ActionConditional<U, V, W, X> {
     const fn new(condition: V, true_branch: W, false_branch: X) -> Self {
         Self {
             condition,
@@ -102,7 +95,7 @@ impl<U: Debug + Send + Sync, V: Action<bool>, W: Action<U>, X: Action<U>>
  * Implement the conditional logic for the ActionConditional action.
  */
 #[async_trait]
-impl<U: Debug + Send + Sync, V: Action<bool>, W: Action<U>, X: Action<U>> Action<U>
+impl<U: Send + Sync, V: Action<bool>, W: Action<U>, X: Action<U>> Action<U>
     for ActionConditional<U, V, W, X>
 {
     async fn execute(self) -> U {
@@ -116,7 +109,7 @@ impl<U: Debug + Send + Sync, V: Action<bool>, W: Action<U>, X: Action<U>> Action
 
 #[derive(Debug)]
 /**
- * Action that runs two actions at the same time and exits both when one exits 
+ * Action that runs two actions at the same time and exits both when one exits
  */
 pub struct RaceAction<T: Action<bool>, U: Action<bool>> {
     first: T,
@@ -124,7 +117,7 @@ pub struct RaceAction<T: Action<bool>, U: Action<bool>> {
 }
 
 /**
- * Construct race action 
+ * Construct race action
  */
 impl<T: Action<bool>, U: Action<bool>> RaceAction<T, U> {
     const fn new(first: T, second: U) -> Self {
@@ -142,7 +135,6 @@ impl<T: Action<bool>, U: Action<bool>> Action<bool> for RaceAction<T, U> {
     }
 }
 
-
 /**
  * Run two actions at once, and only exit when all actions have exited.
  */
@@ -153,7 +145,7 @@ pub struct DualAction<T: Action<bool>, U: Action<bool>> {
 }
 
 /**
- * Constructor for the dual action 
+ * Constructor for the dual action
  */
 impl<T: Action<bool>, U: Action<bool>> DualAction<T, U> {
     const fn new(first: T, second: U) -> Self {
@@ -172,21 +164,14 @@ impl<T: Action<bool>, U: Action<bool>> Action<bool> for DualAction<T, U> {
 }
 
 #[derive(Debug)]
-pub struct ActionChain<
-    T: Debug + Send + Sync,
-    U: Debug + Send + Sync,
-    V: Action<T>,
-    W: ActionMod<T, U>,
-> {
+pub struct ActionChain<T: Send + Sync, U: Send + Sync, V: Action<T>, W: ActionMod<T, U>> {
     first: V,
     second: W,
     _phantom_t: PhantomData<T>,
     _phantom_u: PhantomData<U>,
 }
 
-impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: ActionMod<T, U>>
-    ActionChain<T, U, V, W>
-{
+impl<T: Send + Sync, U: Send + Sync, V: Action<T>, W: ActionMod<T, U>> ActionChain<T, U, V, W> {
     const fn new(first: V, second: W) -> Self {
         Self {
             first,
@@ -198,7 +183,7 @@ impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: ActionMod<
 }
 
 #[async_trait]
-impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: ActionMod<T, U>> Action<U>
+impl<T: Send + Sync, U: Send + Sync, V: Action<T>, W: ActionMod<T, U>> Action<U>
     for ActionChain<T, U, V, W>
 {
     async fn execute(mut self) -> U {
@@ -208,21 +193,14 @@ impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: ActionMod<
 }
 
 #[derive(Debug)]
-pub struct ActionSequence<
-    T: Debug + Send + Sync,
-    U: Debug + Send + Sync,
-    V: Action<T>,
-    W: Action<U>,
-> {
+pub struct ActionSequence<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> {
     first: V,
     second: W,
     _phantom_t: PhantomData<T>,
     _phantom_u: PhantomData<U>,
 }
 
-impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>>
-    ActionSequence<T, U, V, W>
-{
+impl<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> ActionSequence<T, U, V, W> {
     const fn new(first: V, second: W) -> Self {
         Self {
             first,
@@ -234,7 +212,7 @@ impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>>
 }
 
 #[async_trait]
-impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>> Action<(T, U)>
+impl<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> Action<(T, U)>
     for ActionSequence<T, U, V, W>
 {
     async fn execute(self) -> (T, U) {
@@ -243,21 +221,14 @@ impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>>
 }
 
 #[derive(Debug)]
-pub struct ActionParallel<
-    T: Debug + Send + Sync,
-    U: Debug + Send + Sync,
-    V: Action<T>,
-    W: Action<U>,
-> {
+pub struct ActionParallel<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> {
     first: V,
     second: W,
     _phantom_t: PhantomData<T>,
     _phantom_u: PhantomData<U>,
 }
 
-impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>>
-    ActionParallel<T, U, V, W>
-{
+impl<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> ActionParallel<T, U, V, W> {
     const fn new(first: V, second: W) -> Self {
         Self {
             first,
@@ -270,8 +241,8 @@ impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>>
 
 #[async_trait]
 impl<
-        T: 'static + Debug + Send + Sync,
-        U: 'static + Debug + Send + Sync,
+        T: 'static + Send + Sync,
+        U: 'static + Send + Sync,
         V: 'static + Action<T>,
         W: 'static + Action<U>,
     > Action<(T, U)> for ActionParallel<T, U, V, W>
@@ -284,21 +255,14 @@ impl<
 }
 
 #[derive(Debug)]
-pub struct ActionConcurrent<
-    T: Debug + Send + Sync,
-    U: Debug + Send + Sync,
-    V: Action<T>,
-    W: Action<U>,
-> {
+pub struct ActionConcurrent<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> {
     first: V,
     second: W,
     _phantom_t: PhantomData<T>,
     _phantom_u: PhantomData<U>,
 }
 
-impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>>
-    ActionConcurrent<T, U, V, W>
-{
+impl<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> ActionConcurrent<T, U, V, W> {
     const fn new(first: V, second: W) -> Self {
         Self {
             first,
@@ -310,12 +274,10 @@ impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>>
 }
 
 #[async_trait]
-impl<T: Debug + Send + Sync, U: Debug + Send + Sync, V: Action<T>, W: Action<U>> Action<(T, U)>
+impl<T: Send + Sync, U: Send + Sync, V: Action<T>, W: Action<U>> Action<(T, U)>
     for ActionConcurrent<T, U, V, W>
 {
     async fn execute(self) -> (T, U) {
         join!(self.first.execute(), self.second.execute())
     }
 }
-
-
