10cd5f52086b57d87d98f2b1fc66ad17a6fb0ba7
Bennett Petzold <bennettpetzold01@gmail.com>
2023-10-16 19:04:59 -0400

    Add comments to example actions

--- src/missions/basic.rs
+++ src/missions/basic.rs
@@ -4,6 +4,10 @@ use super::{
     movement::Descend,
 };
 
+/// Example function for Action system
+///
+/// Runs two nested actions in order: Waiting for arm and descending in
+/// parallel, followed by waiting for arm and descending concurrently.
 fn initial_descent<T: Send + Sync>(context: &T) -> impl Action + '_ {
     ActionSequence::<T, T, _, _>::new(
         ActionParallel::<T, T, _, _>::new(WaitArm::new(context), Descend::new(context, -0.5)),
