a55bd62d9124dc457a976b22e6560e3171754a3d
Bennett Petzold <bennettpetzold01@gmail.com>
2023-10-23 13:42:31 -0400

    Make example function public

--- src/missions/basic.rs
+++ src/missions/basic.rs
@@ -8,7 +8,7 @@ use super::{
 ///
 /// Runs two nested actions in order: Waiting for arm and descending in
 /// parallel, followed by waiting for arm and descending concurrently.
-fn initial_descent<T: Send + Sync>(context: &T) -> impl Action + '_ {
+pub fn initial_descent<T: Send + Sync>(context: &T) -> impl Action + '_ {
     ActionSequence::<T, T, _, _>::new(
         ActionParallel::<T, T, _, _>::new(WaitArm::new(context), Descend::new(context, -0.5)),
         ActionConcurrent::<T, T, _, _>::new(WaitArm::new(context), Descend::new(context, -1.0)),
