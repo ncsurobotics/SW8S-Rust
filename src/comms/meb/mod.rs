aead514a78851bca78682e5390af054889301b66
Vincent Wu <vwu1888@gmail.com>
2023-11-11 19:57:38 -0500

    Added debouncing to arming

--- src/comms/meb/mod.rs
+++ src/comms/meb/mod.rs
@@ -1,23 +1,31 @@
-use self::response::Statuses;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use anyhow::Result;
+use itertools::Itertools;
 use tokio::io::AsyncReadExt;
 use tokio_serial::{DataBits, Parity, SerialStream, StopBits};
+
+use self::response::Statuses;
 
 pub mod response;
 
 #[derive(Debug)]
 pub struct MainElectronicsBoard {
     statuses: Statuses,
+    arm_count: Arc<Mutex<Vec<bool>>>,
+    arm_state: Arc<Mutex<Option<bool>>>,
 }
 
 impl MainElectronicsBoard {
     pub async fn new<T>(read_connection: T) -> Self
-    where
-        T: 'static + AsyncReadExt + Unpin + Send,
+        where
+            T: 'static + AsyncReadExt + Unpin + Send,
     {
         Self {
             statuses: Statuses::new(read_connection).await,
+            arm_count: Arc::new(Mutex::new(vec![false; 24])),
+            arm_state: Arc::new(Mutex::new(Some(false))),
         }
     }
 
@@ -49,7 +57,24 @@ impl MainElectronicsBoard {
     }
 
     pub async fn thruster_arm(&self) -> Option<bool> {
-        *self.statuses.thruster_arm().read().await
+        let arm_count = self.arm_count.clone();
+        let arm_state = self.arm_state.clone();
+        let current_arm_state = self.arm_state.lock().unwrap().clone();
+
+        let t1 = thread::spawn(move || {
+            let mut locked_arm_count = arm_count.lock().unwrap();
+            let mut locked_arm_state = arm_state.lock().unwrap();
+
+            locked_arm_count.push(current_arm_state.unwrap_or(false));
+            locked_arm_count.remove(0);
+
+            if locked_arm_count.iter().all_equal() {
+                *locked_arm_state = current_arm_state;
+            }
+        });
+
+        t1.join().unwrap();
+        self.arm_state.lock().unwrap().clone()
     }
 
     pub async fn system_voltage(&self) -> Option<f32> {
