9098d60265e3d6c4f4a5366b4de450be1c12ed09
Bennett Petzold <bennettpetzold01@gmail.com>
2023-11-12 22:25:00 -0500

    Create output directory for graphs

--- src/graph_main.rs
+++ src/graph_main.rs
@@ -1,3 +1,5 @@
+use std::fs::create_dir_all;
+
 use futures::{stream, StreamExt};
 use sw8s_rust_lib::missions::action_context::EmptyActionContext;
 use sw8s_rust_lib::missions::graph::{dot_file, draw_svg};
@@ -12,14 +14,18 @@ use generated_actions::descend_and_go_forward;
 #[tokio::main]
 async fn main() {
     let context = EmptyActionContext;
+    create_dir_all("graphs/").unwrap();
     // (name, action) pairs to draw
     let actions = vec![("descend_and_go_forward", descend_and_go_forward(&context))];
 
     stream::iter(actions)
         .for_each(|(name, act)| async move {
             let (res1, res2) = join!(
-                write(name.to_string() + ".svg", draw_svg(&act).unwrap()),
-                write(name.to_string() + ".dot", dot_file(&act))
+                write(
+                    "graphs/".to_string() + &name + ".svg",
+                    draw_svg(&act).unwrap()
+                ),
+                write("graphs/".to_string() + &name + ".dot", dot_file(&act))
             );
             res1.unwrap();
             res2.unwrap();
