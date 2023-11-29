567a9232fab670de823e7ea8d6c222cd553f7d25
Vincent Wu <vwu1888@gmail.com>
2023-11-29 00:15:51 -0500

    Moved arm debouncing to Statuses

--- src/comms/meb/mod.rs
+++ src/comms/meb/mod.rs
@@ -1,12 +1,7 @@
-use std::sync::{Arc};
-use std::thread;
-
 use anyhow::Result;
 use itertools::Itertools;
 use tokio::io::AsyncReadExt;
-use tokio::join;
 use tokio_serial::{DataBits, Parity, SerialStream, StopBits};
-use tokio::sync::Mutex;
 
 use self::response::Statuses;
 
@@ -15,8 +10,6 @@ pub mod response;
 #[derive(Debug)]
 pub struct MainElectronicsBoard {
     statuses: Statuses,
-    arm_count: Arc<Mutex<Vec<bool>>>,
-    arm_state: Arc<Mutex<Option<bool>>>,
 }
 
 impl MainElectronicsBoard {
@@ -26,8 +19,6 @@ impl MainElectronicsBoard {
     {
         Self {
             statuses: Statuses::new(read_connection).await,
-            arm_count: Arc::new(Mutex::new(vec![false; 24])),
-            arm_state: Arc::new(Mutex::new(Some(false))),
         }
     }
 
@@ -59,24 +50,7 @@ impl MainElectronicsBoard {
     }
 
     pub async fn thruster_arm(&self) -> Option<bool> {
-        let arm_count = self.arm_count.clone();
-        let arm_state = self.arm_state.clone();
-        let current_arm_state = *self.statuses.thruster_arm().read().await;
-
-        let mut t1 = tokio::spawn(async move {
-            let mut locked_arm_count = arm_count.lock().await;
-            let mut locked_arm_state = arm_state.lock().await;
-
-            locked_arm_count.push(current_arm_state.unwrap_or(false));
-            locked_arm_count.remove(0);
-
-            if locked_arm_count.iter().all_equal() {
-                *locked_arm_state = current_arm_state;
-            }
-        });
-
-        join!(t1);
-        self.arm_state.lock().await.clone()
+        *self.statuses.thruster_arm().read().await
     }
 
     pub async fn system_voltage(&self) -> Option<f32> {
@@ -85,68 +59,5 @@ impl MainElectronicsBoard {
 
     pub async fn shutdown_cause(&self) -> Option<u8> {
         *self.statuses.shutdown().read().await
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[tokio::test]
-    async fn thruster_is_armed() {
-        let meb = MainElectronicsBoard::new(tokio::io::empty()).await;
-
-        // Receive 24 consecutive messages with thruster arm set to true
-        for _ in 0..24 {
-            meb.statuses.thruster_arm().write().await.replace(true);
-            meb.thruster_arm().await;
-        }
-
-        assert_eq!(meb.thruster_arm().await, Some(true));
-    }
-
-    #[tokio::test]
-    async fn thruster_is_not_armed() {
-        let meb = MainElectronicsBoard::new(tokio::io::empty()).await;
-
-        // Receive 24 consecutive messages with thruster arm set to false
-        for _ in 0..24 {
-            meb.statuses.thruster_arm().write().await.replace(false);
-            meb.thruster_arm().await;
-        }
-
-        assert_eq!(meb.thruster_arm().await, Some(false));
-    }
-
-    #[tokio::test]
-    async fn thrust_arm_debounce() {
-        let meb = MainElectronicsBoard::new(tokio::io::empty()).await;
-
-        meb.statuses.thruster_arm().write().await.replace(false);
-        assert_eq!(meb.thruster_arm().await, Some(false));
-
-        for _ in 0..10 {
-            meb.statuses.thruster_arm().write().await.replace(true);
-            meb.thruster_arm().await;
-        }
-        assert_eq!(meb.thruster_arm().await, Some(false));
-
-        for _ in 0..24 {
-            meb.statuses.thruster_arm().write().await.replace(true);
-            meb.thruster_arm().await;
-        }
-        assert_eq!(meb.thruster_arm().await, Some(true));
-
-        for _ in 0..10 {
-            meb.statuses.thruster_arm().write().await.replace(false);
-            meb.thruster_arm().await;
-        }
-        assert_eq!(meb.thruster_arm().await, Some(true));
-
-        for _ in 0..24 {
-            meb.statuses.thruster_arm().write().await.replace(false);
-            meb.thruster_arm().await;
-        }
-        assert_eq!(meb.thruster_arm().await, Some(false));
     }
 }
