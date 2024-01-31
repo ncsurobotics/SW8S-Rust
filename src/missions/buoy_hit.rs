d9beb9e06a3c49f77ac0ae4d804729458d7b707b
Ben Caunt <bdcaunt@gmail.com>
2024-01-30 20:55:35 -0500

    split up buoy algorithm to take advantage of command structure

--- src/missions/buoy_hit.rs
+++ src/missions/buoy_hit.rs
@@ -1,10 +1,10 @@
-use crate::vision::{buoy::Buoy, VisualDetector};
+use crate::vision::{buoy::Buoy, nn_cv2::OnnxModel, VisualDetector};
 
 use super::{
-    action::{Action, ActionExec, ActionSequence},
+    action::{Action, ActionExec, ActionSequence, ActionWhile},
     action_context::{GetControlBoard, GetFrontCamMat},
     basic::DelayAction,
-    movement::ZeroMovement,
+    movement::{StraightMovement, ZeroMovement},
 };
 
 use anyhow::Result;
@@ -22,6 +22,7 @@ pub struct DriveToBuoyVision<'a, T> {
     target_depth: f32,
     forward_power: f32,
     k_p: f32,
+    buoy_model: Buoy<OnnxModel>,
 }
 
 impl<'a, T> DriveToBuoyVision<'a, T> {
@@ -31,6 +32,7 @@ impl<'a, T> DriveToBuoyVision<'a, T> {
             target_depth,
             forward_power,
             k_p: 0.3,
+            buoy_model: Buoy::default(),
         }
     }
 }
@@ -43,62 +45,51 @@ where
     T: GetControlBoard<WriteHalf<SerialStream>> + GetFrontCamMat + Sync + Unpin,
 {
     type Output = Result<()>;
+
     async fn execute(&mut self) -> Self::Output {
-        let mut buoy_model = Buoy::default();
+        let camera_aquisition = self.context.get_front_camera_mat();
         let class_of_interest = self.context.get_desired_buoy_gate().await;
-        println!("Getting control board and setting speed to zero before buoy search.");
-        self.context
-            .get_control_board()
-            .stability_2_speed_set_initial_yaw(0.0, 0.0, 0.0, 0.0, self.target_depth)
-            .await?;
-        println!("GOT ZERO SPEED SET");
 
-        let mut can_see_buoy = true;
+        let model_acquisition = self.buoy_model.detect(&camera_aquisition.await);
+        match model_acquisition {
+            Ok(acquisition_vec) if !acquisition_vec.is_empty() => {
+                let detected_item = acquisition_vec
+                    .iter()
+                    .find(|&result| *result.class() == class_of_interest);
 
-        while can_see_buoy {
-            let camera_aquisition = self.context.get_front_camera_mat();
-            let model_acquisition = buoy_model.detect(&camera_aquisition.await);
-            match model_acquisition {
-                Ok(acquisition_vec) => {
-                    if acquisition_vec.is_empty() {
-                        can_see_buoy = false;
-                    }
-                    let detected_item = acquisition_vec
-                        .iter()
-                        .find(|&result| *result.class() == class_of_interest);
-                    match detected_item {
-                        Some(scan) => {
-                            let position = buoy_model.normalize(scan.position());
-                            self.context
-                                .get_control_board()
-                                .stability_2_speed_set_initial_yaw(
-                                    self.forward_power,
-                                    self.k_p * position.x as f32,
-                                    0.0,
-                                    0.0,
-                                    self.target_depth,
-                                )
-                                .await?;
-                        }
-                        None => can_see_buoy = false,
-                    }
+                if let Some(scan) = detected_item {
+                    let position = self.buoy_model.normalize(scan.position());
+                    self.context
+                        .get_control_board()
+                        .stability_2_speed_set_initial_yaw(
+                            self.forward_power,
+                            self.k_p * position.x as f32,
+                            0.0,
+                            0.0,
+                            self.target_depth,
+                        )
+                        .await?;
+                    Ok(()) // Repeat the action
+                } else {
+                    Err(anyhow::format_err!("no longer detected")) // Stop the action
                 }
-                Err(_) => return Ok(()),
+            }
+            _ => {
+                Err(anyhow::format_err!(
+                    "No buoy detected or error in detection"
+                )) // Stop the action
             }
         }
-        // once we cannot see the buoy, we still want to continue forward, this implies we need to set the power to zero later.
-        self.context
-            .get_control_board()
-            .stability_2_speed_set_initial_yaw(self.forward_power, 0.0, 0.0, 0.0, self.target_depth)
-            .await?;
-        Ok(())
     }
 }
 
 pub fn buoy_bump_sequence<'a, T>(
     context: &'a T,
     depth: f32,
-) -> ActionSequence<DriveToBuoyVision<'a, T>, ActionSequence<DelayAction, ZeroMovement<'a, T>>>
+) -> ActionSequence<
+    ActionWhile<DriveToBuoyVision<'a, T>>,
+    ActionSequence<StraightMovement<T>, ActionSequence<DelayAction, ZeroMovement<'a, T>>>,
+>
 where
     T: GetControlBoard<WriteHalf<SerialStream>> + 'a,
 {
@@ -107,7 +98,9 @@ where
 
     // Instantiate DriveToBuoyVision with provided values
     let drive_to_buoy_vision = DriveToBuoyVision::new(context, depth, forward_power);
+    let drive_while_buoy_visible = ActionWhile::new(drive_to_buoy_vision);
 
+    let forward_action = StraightMovement::new(context, depth, true);
     // Create a DelayAction with hardcoded delay
     let delay_action = DelayAction::new(delay_s);
 
@@ -115,8 +108,10 @@ where
     let zero_movement = ZeroMovement::new(context, depth);
 
     // Create the inner ActionSequence
-    let inner_sequence = ActionSequence::new(delay_action, zero_movement);
-
+    let inner_sequence = ActionSequence::new(
+        forward_action,
+        ActionSequence::new(delay_action, zero_movement),
+    );
     // Create and return the outer ActionSequence
-    ActionSequence::new(drive_to_buoy_vision, inner_sequence)
+    ActionSequence::new(drive_while_buoy_visible, inner_sequence)
 }
