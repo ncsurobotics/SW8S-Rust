c084a16c019634c9f4c69c61a56a068d1fd7ec61
Bennett-Petzold <46354948+Bennett-Petzold@users.noreply.github.com>
2024-04-09 00:55:37 +0000

    Deploying to gh-pages from @ ncsurobotics/SW8S-Rust@264180b25976dfe3749f8f6699e926a1d82f5327 🚀

--- docs/pool-test-3-30/sw8s_rust_lib/vision/path/sidebar-items.js
+++ docs/pool-test-3-30/sw8s_rust_lib/vision/path/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["compute_angle"],"static":["FORWARD"],"struct":["Path","Yuv"]};
+window.SIDEBAR_ITEMS = {"fn":["compute_angle"],"static":["FORWARD"],"struct":["MatWrapper","Path","Yuv"]};
