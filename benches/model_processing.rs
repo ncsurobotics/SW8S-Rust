6e51e8a918f0059a1e6a673ef01359ae3bfa0e89
Bennett Petzold <bennettpetzold01@gmail.com>
2024-06-13 16:27:49 -0400

    Add buoy model for benchmarking

--- benches/model_processing.rs
+++ benches/model_processing.rs
@@ -1,6 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use opencv::imgcodecs::{imread, IMREAD_COLOR};
-use sw8s_rust_lib::vision::{gate_poles::GatePoles, VisualDetector};
+use sw8s_rust_lib::vision::{
+    buoy::Buoy, buoy_model::BuoyModel, gate_poles::GatePoles, VisualDetector,
+};
 
 const CUDA_ENABLED: &str = if cfg!(feature = "cuda") {
     if cfg!(feature = "cuda_f16") {
@@ -32,21 +34,18 @@ fn gate_pole_model(c: &mut Criterion) {
 
 fn buoy_model(c: &mut Criterion) {
     let image = imread(
-        "tests/vision/resources/gate_images/straight_on_0.png",
+        "tests/vision/resources/buoy_images/straight_on_0.png",
         IMREAD_COLOR,
     )
     .unwrap();
-    let mut model = GatePoles::default();
+    let mut model = BuoyModel::default();
 
-    c.bench_function(
-        &("Gate Pole Model (".to_string() + CUDA_ENABLED + ")"),
-        |b| {
-            b.iter(|| {
-                black_box(model.detect(&image).unwrap());
-            })
-        },
-    );
+    c.bench_function(&("Buoy Model (".to_string() + CUDA_ENABLED + ")"), |b| {
+        b.iter(|| {
+            black_box(model.detect(&image).unwrap());
+        })
+    });
 }
 
-criterion_group!(model_processing, gate_pole_model);
+criterion_group!(model_processing, gate_pole_model, buoy_model);
 criterion_main!(model_processing);
