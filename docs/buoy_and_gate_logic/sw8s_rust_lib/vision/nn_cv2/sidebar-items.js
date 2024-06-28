e142fa565a5defc2c5d38eddae80b71c1f4e8273
Bennett-Petzold <46354948+Bennett-Petzold@users.noreply.github.com>
2024-06-28 19:59:56 +0000

    Deploying to gh-pages from @ ncsurobotics/SW8S-Rust@4f42a1c8821ab5024c9d9bf205c39ff62ad3616b 🚀

---
+++ docs/buoy_and_gate_logic/sw8s_rust_lib/vision/nn_cv2/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ModelPipelined","ModelPipelinedInput","NetWrapper","OnnxModel","YoloClass","YoloDetection"],"trait":["VisionModel"]};
---
+++ docs/buoy_and_gate_logic/trait.impl/concurrent_queue/sync/prelude/trait.UnsafeCellExt.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"concurrent_queue":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
