1565be72c44a8fa27bd8ff0d19f466805e5db490
Bennett Petzold <dansecob.dev@gmail.com>
2024-08-09 15:16:18 -0400

    Increase circle count

--- src/missions/circle_buoy.rs
+++ src/missions/circle_buoy.rs
@@ -155,7 +155,7 @@ pub fn buoy_circle_sequence_blind<
     const BUOY_YAW_SPEED: f32 = -12.0;
     const DEPTH: f32 = -1.5;
     const DESCEND_WAIT_DURATION: f32 = 3.0;
-    const CIRCLE_COUNT: u32 = 28;
+    const CIRCLE_COUNT: u32 = 34;
 
     act_nest!(
         ActionSequence::new,
