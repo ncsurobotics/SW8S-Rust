84de2481b5bba106c87602ccdfff0a2c15b46cda
SarangaR <sarnga.raj@gmail.com>
2024-10-25 16:03:21 +0000

    devcontainer fixes and seaowlf script addition

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -8,6 +8,7 @@ echo -e "\n----COMMANDS----"
 echo "seawolf build          : Build the project (local)"
 echo "seawolf build jetson   : Cross compile for jetson"
 echo "seawolf build local    : Compile for local machine (x86_64)"
+echo "seawolf run            : Run the project (local build + run)"
 
 echo -e "\n----Build Command----"
 echo -e "ctrl + shift + b   : Build the project for jetson"
