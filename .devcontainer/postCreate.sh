ecb86dc95e763f27d447e32453797de7e93ae28d
SarangaR <sarnga.raj@gmail.com>
2024-10-25 16:02:44 +0000

    devcontainer fixes and seaowlf script addition

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "export PATH="/workspaces/SW8S-Rust/scripts:$PATH"" >> ~/.bashrc
--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+source ~/.bashrc
+
 echo -e "Setting up the environment..."
 
 echo -e "\n----COMMANDS----"
-echo "seawolf build          : Build the project"
+echo "seawolf build          : Build the project (local)"
 echo "seawolf build jetson   : Cross compile for jetson"
 echo "seawolf build local    : Compile for local machine (x86_64)"
 
 echo -e "\n----Build Command----"
-echo -e "ctrl + shift + b   : Build the project"
-echo -e "ctrl + shift + j   : Cross compile for jetson"
+echo -e "ctrl + shift + b   : Build the project for jetson"
