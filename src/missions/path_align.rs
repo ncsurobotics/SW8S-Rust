a705f100124eb42680701a26a8f90d4cc67c900b
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-05-08 04:40:32 -0400

    feat: procedural path align implementation - Terminates based on the number of consecutive positive path detections   - We should consider replacing this with an average angle error ceiling

--- src/missions/path_align.rs
+++ src/missions/path_align.rs
@@ -172,37 +172,76 @@ pub async fn path_align_procedural<
 ) {
     const DEPTH: f32 = -1.25;
     const PATH_ALIGN_SPEED: f32 = 0.3;
+    const DETECTIONS: u8 = 10;
 
-    let mut visionNormBottom =
+    #[cfg(feature = "logging")]
+    logln!("Starting path align");
+
+    let cb = context.get_control_board();
+    let mut vision_norm_bottom =
         VisionNormBottomAngle::<Con, PathCV, f64>::new(context, PathCV::default());
-    loop {
-        let detections = visionNormBottom.execute().await.unwrap();
-        if let Some(detection) = detections
-            .into_iter()
-            .filter_map(|d| {
-                if *d.class() {
-                    Some(d.position().clone())
-                } else {
-                    None
-                }
-            })
-            .next()
-        {
-            let x = *detection.x() as f32;
-            let y = (*detection.y() as f32) * -1.0;
-            let angle = detection.angle();
-            let cb = context.get_control_board();
-            if let Some(current_angle) = cb.responses().get_angles().await {
-                cb.stability_2_speed_set(
-                    x,
-                    y,
-                    0.0,
-                    0.0,
-                    (*current_angle.yaw() as f32) + *angle as f32,
-                    DEPTH,
-                )
-                .await;
-            }
+
+    let initial_yaw = loop {
+        if let Some(initial_angle) = cb.responses().get_angles().await {
+            break *initial_angle.yaw() as f32;
+        } else {
+            #[cfg(feature = "logging")]
+            logln!("Failed to get initial angle");
         }
+    };
+
+    cb.stability_2_speed_set(0.0, PATH_ALIGN_SPEED, 0.0, 0.0, initial_yaw, DEPTH)
+        .await;
+
+    let mut last_set_yaw = initial_yaw;
+    let mut consec_detections = 0;
+
+    #[cfg(feature = "logging")]
+    logln!("Starting path detection");
+
+    loop {
+        if consec_detections >= DETECTIONS {
+            #[cfg(feature = "logging")]
+            logln!("Finished path align");
+
+            break;
+        }
+
+        if let Some(current_angle) = cb.responses().get_angles().await {
+            let current_yaw = *current_angle.yaw() as f32;
+
+            // For the opencv impl of path detection, the returned vector is guaranteed to contain 1 item
+            let detections = vision_norm_bottom.execute().await.unwrap_or_else(|e| {
+                #[cfg(feature = "logging")]
+                logln!(
+                    "Getting path detection resulted in error: `{e}`\n\tUsing empty detection vec"
+                );
+                vec![]
+            });
+
+            let mut positions = detections
+                .into_iter()
+                .filter_map(|d| d.class().then_some(d.position().clone()));
+
+            if let Some(position) = positions.next() {
+                let x = *position.x() as f32;
+                let y = (*position.y() as f32) * -1.0;
+                let angle = position.angle();
+                last_set_yaw = current_yaw + *angle as f32;
+                cb.stability_2_speed_set(x, y, 0.0, 0.0, last_set_yaw, DEPTH)
+                    .await;
+                consec_detections += 1;
+            } else {
+                cb.stability_2_speed_set(0.0, PATH_ALIGN_SPEED, 0.0, 0.0, last_set_yaw, DEPTH)
+                    .await;
+                consec_detections = 0;
+            }
+        } else {
+            #[cfg(feature = "logging")]
+            logln!("Failed to get current angle");
+        }
+
+        #[cfg(feature = "logging")]
+        logln!("Positive detection count: {consec_detections}");
     }
 }
