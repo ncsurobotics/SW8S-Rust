d93b46d127bacee410bd29c8130a552cdfd51fed
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-08-18 12:45:08 -0400

    correct slalom mission names

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ while true; do
   case $1 in
     1)
       ln -f -s config_1.toml config.toml
-      ./sw8s_rust arm gate_run_reckon spin left_slalom left_slalom left_slalom static_align
+      ./sw8s_rust arm gate_run_reckon spin slalom_left slalom_left slalom_left static_align
     ;;
     2)
       ln -f -s config_2.toml config.toml
-      ./sw8s_rust arm gate_run_reckon spin left_slalom right_slalom right_slalom static_align
+      ./sw8s_rust arm gate_run_reckon spin slalom_left slalom_right slalom_right static_align
     ;;
     3)
       ln -f -s config_3.toml config.toml
